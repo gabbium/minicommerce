081ea5404d57435716a91d1c9c28e2d1c99bba56
Gabriel Abreu <gabriel@gabbium.dev>
2025-08-23 17:41:37 -0300

    test: add swagger acceptance tests

---
+++ identity/tests/MiniCommerce.Identity.Web.AcceptanceTests/Endpoints/Swagger/GetSwaggerSteps.cs
@@ -0,0 +1,22 @@
+﻿using MiniCommerce.Identity.Web.AcceptanceTests.Steps;
+using MiniCommerce.Identity.Web.AcceptanceTests.TestHelpers;
+
+namespace MiniCommerce.Identity.Web.AcceptanceTests.Endpoints.Swagger;
+
+public class GetSwaggerSteps(TestFixture fixture) : CommonStepsBase(fixture)
+{
+    public async Task WhenTheyAttemptToChecksSwaggerJson()
+    {
+        HttpResponse = await Fixture.Client.GetAsync("/api/specification.json");
+    }
+
+    public async Task WhenTheyAttemptToChecksSwaggerUi()
+    {
+        HttpResponse = await Fixture.Client.GetAsync("/api");
+    }
+
+    public async Task WhenTheyAttemptToChecksSwaggerRedocs()
+    {
+        HttpResponse = await Fixture.Client.GetAsync("/docs");
+    }
+}
